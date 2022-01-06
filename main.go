9b5774515b4f382a126df742af8b0ba5b31450ba
Mert Can Yilmaz <bestrasen@hotmail.com>
2022-01-06 17:47:16 +0300

    tutorial demo

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 	"example.com/go-sandbox/config"
 	"example.com/go-sandbox/controller"
+	"example.com/go-sandbox/middleware"
 	"example.com/go-sandbox/repository"
 	"example.com/go-sandbox/service"
 
@@ -31,9 +32,12 @@ func handleRequests() {
 var (
 	db             *gorm.DB                  = config.SetupDatabaseConnection()
 	userRepository repository.UserRepository = repository.NewUserRepository(db)
+	bookRepository repository.BookRepository = repository.NewBookRepository(db)
 	jwtService     service.JWTService        = service.NewJWTService()
 	authService    service.AuthService       = service.NewAuthService(userRepository)
+	bookService    service.BookService       = service.NewBookService(bookRepository)
 	authController controller.AuthController = controller.NewAuthController(authService, jwtService)
+	bookController controller.BookController = controller.NewBookController(bookService, jwtService)
 )
 
 func main() {
@@ -48,6 +52,14 @@ func main() {
 	{
 		authRoutes.POST("/login", authController.Login)
 		authRoutes.POST("/register", authController.Register)
+	}
+	bookRoutes := r.Group("api/books", middleware.AuthorizeJWT(jwtService))
+	{
+		bookRoutes.GET("/", bookController.All)
+		bookRoutes.POST("/", bookController.Insert)
+		bookRoutes.GET("/:id", bookController.FindByID)
+		bookRoutes.PUT("/:id", bookController.Update)
+		bookRoutes.DELETE("/:id", bookController.Delete)
 	}
 
 	r.Run()
