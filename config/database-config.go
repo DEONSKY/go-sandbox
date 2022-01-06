c2af0c4943e79e91c9bd3b276b3156a856f93aa9
DEONSKY <bestrasen@hotmail.com>
2022-01-06 22:27:01 +0300

    ssl mode

--- config/database-config.go
+++ config/database-config.go
@@ -22,7 +22,7 @@ func SetupDatabaseConnection() *gorm.DB {
 	dbHost := os.Getenv("DB_HOST")
 	dbName := os.Getenv("DB_NAME")
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable", dbHost, dbUser, dbPass, dbName)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432", dbHost, dbUser, dbPass, dbName)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Failed to create a connection to database")
